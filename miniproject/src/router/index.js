3cf887ea2dddb40b51b6ea054ce94a8878dbfc63
Mustika Marito Siahaan <mustikasiahaan4@gmail.com>
2022-05-18 13:28:39 +0700

    ups

--- miniproject/src/router/index.js
+++ miniproject/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Menu from '../views/Menu.vue'
 import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact,
   },
 ]
 
---
+++ miniproject/src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <ContactPage/>
+  </div>
+</template>
+
+<script>
+
+import ContactPage from '@/components/ContactPage.vue'
+
+export default {
+  name: 'Contact',
+  components: {
+    ContactPage
+
+}
+}
+</script>
