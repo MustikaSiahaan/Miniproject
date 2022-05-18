123be0b4f42dbf387b30348dc364ab601d9a71a9
Mustika Marito Siahaan <mustikasiahaan4@gmail.com>
2022-05-18 13:23:03 +0700

    up

--- miniproject/src/router/index.js
+++ miniproject/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Menu from '../views/Menu.vue'
-
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -16,6 +16,11 @@ const routes = [
     path: '/menu',
     name: 'Menu',
     component: Menu
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
   },
 ]
 
