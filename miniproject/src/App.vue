f1eee4677b3c69393e815eb59baafcb80cccef6b
Mustika Marito Siahaan <mustikasiahaan4@gmail.com>
2022-05-18 12:55:32 +0700

    new

---
+++ miniproject/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <div>
+    <Navbar />
+    <router-view/>
+      
+      <Footer />
+    </div>
+    <div>
+    
+    </div>
+   
+  </v-app>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+  components: {Navbar, Footer},
+  data(){
+    return{
+      navs:[
+        {title:'Home', route:'home'},
+        {title:'Menu', route:'menu'},
+        {title:'About', route:'about'},
+        {title:'Contact', route:'contact'},
+      ]
+    }
+  }
+};
+</script>
